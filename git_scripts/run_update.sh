f627bf34b5c472b99c096c61e3dd02f286c95d8c
TutuDoan <tung.doan.30692@gmail.com>
2018-11-05 23:33:22 +0100

    fix updating script

--- git_scripts/run_update.sh
+++ git_scripts/run_update.sh
@@ -1,14 +1,14 @@
 cd ..
 cur_branch=$(git branch | grep \* | cut -d ' ' -f2)
+git stash
 if [ "$cur_branch" != "Rel1200" ]
 then
-	git stash
 	git checkout Rel1200
 fi
 git pull
 if [ "$cur_branch" != "Rel1200" ]
 then
-	git stash pop
 	git checkout $cur_branch
 fi
+git stash pop
 cd -
